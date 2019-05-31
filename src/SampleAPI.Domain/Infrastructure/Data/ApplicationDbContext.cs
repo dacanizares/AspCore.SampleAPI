06ee52cc644578e220ba42346a96740b479d406f
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-05-30 22:39:25 -0500

    Create ApplicationDbContext.cs

---
+++ src/SampleAPI.Domain/Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SampleAPI.Domain.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Seed();
+        }
+    }
+}
