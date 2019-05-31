6a46f1a71624c2635557ad61834b9a5adacbeda6
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-05-30 22:40:21 -0500

    Adding Commands

---
+++ src/SampleAPI/Commands/CreateUserCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleAPI.Commands
+{
+    public class CreateUserCommand
+    {
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+    }
+}
---
+++ src/SampleAPI/Commands/UpdateUserCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleAPI.Commands
+{
+    public class UpdateUserCommand
+    {
+        public string Email { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
