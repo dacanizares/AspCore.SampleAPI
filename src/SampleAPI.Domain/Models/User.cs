0f9569340772483e551a462507a932203907b87d
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-05-30 22:39:54 -0500

    Create User.cs

---
+++ src/SampleAPI.Domain/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SampleAPI.Domain
+{
+    public class User
+    {
+        [Key]
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public DateTime? CreatedAt { get;  set; }
+
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
