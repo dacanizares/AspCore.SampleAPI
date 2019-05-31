93b32f27ea27ccd6c5e34c6471ae101fc63ee11c
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-05-30 22:39:29 -0500

    Create SeedExtensions.cs

---
+++ src/SampleAPI.Domain/Infrastructure/Data/SeedExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleAPI.Domain.Infrastructure.Data
+{
+    public static class SeedExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(
+                new User
+                {
+                    Username = "Mr. Sample",
+                    Email = "sample@email.com",
+                    IsActive = true,
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now
+                }
+            );
+        }
+    }
+}
