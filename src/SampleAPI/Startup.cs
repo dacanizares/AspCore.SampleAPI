f4607452c0f6495341bba7f22bb54ad95c964ec0
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-05-30 22:46:00 -0500

    Adding swashbuckle

--- src/SampleAPI/Startup.cs
+++ src/SampleAPI/Startup.cs
@@ -15,6 +15,7 @@ using Microsoft.Extensions.Options;
 using SampleAPI.Domain.Infrastructure.Data;
 using SampleAPI.Domain.Infrastructure.Repositories;
 using SampleAPI.Domain.Managers;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace SampleAPI
 {
@@ -44,6 +45,12 @@ namespace SampleAPI
 
             // Add Automapper
             services.AddAutoMapper();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "SampleAPI", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,6 +65,17 @@ namespace SampleAPI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "SampleAPI V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
