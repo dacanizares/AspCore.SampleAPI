9049568e250c663e27c64e3233207ef283f008e7
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-05-30 22:40:28 -0500

    Create UserMappings.cs

---
+++ src/SampleAPI/Mappings/UserMappings.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using SampleAPI.Commands;
+using SampleAPI.Domain;
+
+namespace SampleAPI.Mappings
+{
+    public class UserMappings : Profile
+    {
+        public UserMappings()
+        {
+            CreateMap<CreateUserCommand, User>();
+            CreateMap<UpdateUserCommand, User>();
+        }
+    }
+}
