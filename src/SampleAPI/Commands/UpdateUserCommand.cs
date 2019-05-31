1e5d15461edfb64adc5c16d43e69ca541fa08fda
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-05-31 13:40:12 -0500

    Adding validation for commands

--- src/SampleAPI/Commands/UpdateUserCommand.cs
+++ src/SampleAPI/Commands/UpdateUserCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace SampleAPI.Commands
     {
         public string Email { get; set; }
 
+        [Required]
         public bool IsActive { get; set; }
     }
 }
