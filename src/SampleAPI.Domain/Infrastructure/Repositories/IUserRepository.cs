d317181bac92e67f8d3b0df1f5b55c50ba0e9a6a
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-05-30 22:39:50 -0500

    Create Repositories

---
+++ src/SampleAPI.Domain/Infrastructure/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SampleAPI.Domain.Infrastructure.Repositories
+{
+    public interface IUserRepository
+    {
+        Task CreateUserAsync(User user);
+        Task<List<User>> FindAllAsync();
+        Task<User> FindByUsernameAsync(string username);
+        Task DeleteUserAsync(User user);
+        Task UpdateUserAsync(User user);
+    }
+}
---
+++ src/SampleAPI.Domain/Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SampleAPI.Domain.Infrastructure.Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SampleAPI.Domain.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        protected readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<User>> FindAllAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User> FindByUsernameAsync(string username)
+        {
+            return await _context.Users.FirstOrDefaultAsync(user => user.Username == username);
+        }
+
+        public async Task CreateUserAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateUserAsync(User user)
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteUserAsync(User user)
+        {
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
