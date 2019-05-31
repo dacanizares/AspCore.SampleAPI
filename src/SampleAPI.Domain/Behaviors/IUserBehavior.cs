235a904e7b2c121f7d9d8374a94e8f1349225ab1
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-05-30 22:39:42 -0500

    Create Behaviors

---
+++ src/SampleAPI.Domain/Behaviors/IUserBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SampleAPI.Domain.Managers
+{
+    public interface IUserBehavior
+    {
+        Task CreateUserAsync(User user);
+        Task DeleteUserAsync(User user);
+        Task<List<User>> FindAllAsync();
+        Task<User> FindByUsernameAsync(string username);
+        Task UpdateUserAsync(User user);
+    }
+}
---
+++ src/SampleAPI.Domain/Behaviors/UserBehavior.cs
@@ -0,0 +1,53 @@
+﻿using SampleAPI.Domain.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleAPI.Domain.Managers
+{
+    public class UserBehavior : IUserBehavior
+    {
+        protected readonly IUserRepository _repository;
+
+        public UserBehavior(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<User>> FindAllAsync()
+        {
+            return await _repository.FindAllAsync();
+        }
+
+        public async Task<User> FindByUsernameAsync(string username)
+        {
+            return await _repository.FindByUsernameAsync(username);
+        }
+
+        public async Task CreateUserAsync(User user)
+        {
+            if (user is null) throw new ArgumentNullException(nameof(user));
+
+            user.IsActive = true;
+            user.CreatedAt = DateTime.Now;
+            user.UpdatedAt = DateTime.Now;
+            await _repository.CreateUserAsync(user);
+        }
+
+        public async Task UpdateUserAsync(User user)
+        {
+            if (user is null) throw new ArgumentNullException(nameof(user));
+
+            user.UpdatedAt = DateTime.Now;
+            await _repository.UpdateUserAsync(user);
+        }
+
+        public async Task DeleteUserAsync(User user)
+        {
+            if (user is null) throw new ArgumentNullException(nameof(user));
+
+            await _repository.DeleteUserAsync(user);
+        }
+    }
+}
