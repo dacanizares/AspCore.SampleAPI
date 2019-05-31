177d9943a74c0f7c34bb81c83adcdba1a440ecb8
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-05-30 22:45:36 -0500

    Adding migrations

---
+++ src/SampleAPI/Migrations/20190531034300_Initial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SampleAPI.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Username = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: true),
+                    UpdatedAt = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Username);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Username", "CreatedAt", "Email", "IsActive", "UpdatedAt" },
+                values: new object[] { "Mr. Sample", new DateTime(2019, 5, 30, 22, 43, 0, 58, DateTimeKind.Local).AddTicks(5667), "sample@email.com", true, new DateTime(2019, 5, 30, 22, 43, 0, 59, DateTimeKind.Local).AddTicks(8380) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
